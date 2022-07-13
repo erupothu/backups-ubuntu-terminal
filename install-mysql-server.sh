e3ec98fb53052fc6375d3dd76959fe4f39bfbe75
Erupothu Harish <erupothu@users.noreply.github.com>
2022-07-13 15:38:43 +0530

    installing
    install databases, create dump and restore db dump

---
+++ install-mysql-server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install the latest version of MySql
+sudo apt install -y mysql-server
+
+sudo mysql_secure_installation
+
+sudo mysql -uroot -p -e "CREATE USER cronuser@localhost IDENTIFIED BY 'cronpass';";
+
+sudo mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO 'cronuser'@'localhost';";
+
+#sudo mysql -uroot -p -e "ALTER USER cronuser WITH SUPERUSER;";
+
+#sudo -u postgres psql -c "ALTER USER cronuser WITH PASSWORD 'newpassword';"
+#sudo -u postgres psql -c "GRANT CONNECT ON DATABASE database_name TO username;";
+#sudo -u postgres psql -c "GRANT USAGE ON SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE database_name TO username;";
+#sudo -u postgres psql -c "ALTER USER username CREATEDB;";
+#sudo -u postgres psql -c "ALTER USER myuser WITH SUPERUSER;";
+#sudo -u postgres psql -c "ALTER USER username WITH NOSUPERUSER;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE database_name TO username;";
+
+# pg_dump.exe -U username -d databasename -n schemaname > C:\mylocation\mydumpfilename.dmp
+# pg_dump --no-acl --no-owner -h 13.233.112.218 -U postgres -p 5432 dwh -Ft | aws s3 cp - s3://vaya-postgres-crondb/backup/dwh.tar
+
+# mysqldump -h localhost -P 3306  -u root -p vaya@123 database_name | gzip > /home/vaya-it/Desktop/mysql-backup/dump.sql.gz
+# mysql -u root -p audit_data_bkp_restore < audit_data-2022-07-12.sql
---
+++ install-postgresql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Create the file repository configuration:
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Update the package lists:
+sudo apt-get update
+
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+sudo apt-get -y install postgresql
+
+sudo -u postgres psql -c "CREATE USER cronuser WITH ENCRYPTED PASSWORD 'cronpass';";
+
+sudo -u postgres psql -c "ALTER USER cronuser WITH SUPERUSER;";
+
+#sudo -u postgres psql -c "ALTER USER cronuser WITH PASSWORD 'newpassword';"
+#sudo -u postgres psql -c "GRANT CONNECT ON DATABASE database_name TO username;";
+#sudo -u postgres psql -c "GRANT USAGE ON SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA schema_name TO username;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE database_name TO username;";
+#sudo -u postgres psql -c "ALTER USER username CREATEDB;";
+#sudo -u postgres psql -c "ALTER USER myuser WITH SUPERUSER;";
+#sudo -u postgres psql -c "ALTER USER username WITH NOSUPERUSER;";
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE database_name TO username;";
+
+# psql -U postgres
+#sudo -u postgres psql -c "CREATE ROLE alice4 WITH LOGIN PASSWORD 'securePass1';";
+# psql -U alice4 -W postgres
+
+#sudo -u postgres psql -c "CREATE ROLE dbowner NOLOGIN;";
+#sudo -u postgres psql -c "ALTER DATABASE mydb OWNER TO dbowner;";
+#sudo -u postgres psql -c "GRANT dbowner TO user1, user2;";
+
+# pg_dump.exe -U username -d databasename -n schemaname > C:\mylocation\mydumpfilename.dmp
+# pg_dump --host=localhost --port=5432 --dbname=dwh --username=postgres -d dwh -n nbfc | gzip > nbfc.pgsql.gz
+# pg_dump --no-acl --no-owner -h 13.233.112.218 -U postgres -p 5432 dwh -Ft | aws s3 cp - s3://vaya-postgres-crondb/backup/dwh.tar
+
+# pg_dump --no-acl --no-owner -h 13.233.112.218 -U postgres -p 5432 test -Ft -f /home/vaya-it/Desktop/postgres-backup/test.tar
+# pg_restore --no-acl --no-owner -h localhost -Ft -d postgres -U cronuser /home/vaya-it/Desktop/postgres-backup/test.tar
